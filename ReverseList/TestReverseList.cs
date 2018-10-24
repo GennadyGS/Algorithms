aa57f013785b265589d9d6eb2933223867783448
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-10-24 13:54:17 +0300

    Fix typo

--- ReverseList/TestReverseList.cs
+++ ReverseList/TestReverseList.cs
@@ -9,7 +9,7 @@ namespace ReverseList
         [Theory]
         [InlineData(new[] { 1, 2, 3 }, new[] { 3, 2, 1 })]
         [InlineData(new int[] { }, new int[] { })]
-        void ListShoudBeRevertedCorrectly(IEnumerable<int> input, IEnumerable<int> output)
+        void ListShouldBeRevertedCorrectly(IEnumerable<int> input, IEnumerable<int> output)
         {
             LinkedList<int> inputList = LinkedList.FromEnumerable(input);
             var outputList = inputList.ReverseList();
