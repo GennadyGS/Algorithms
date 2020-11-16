5c990d3590fe26bf15b84b86d0f9396ed962ad8a
Gennadii Saltyshchak <GennadyGS@gmail.com>
2020-11-16 18:51:29 +0200

    Add PrimeFactors

---
+++ PrimeFactors/PrimeFactors.Tests/PrimeFactorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PrimeFactors.Tests
+{
+    public class PrimeFactorTests
+    {
+        private readonly PrimeFactorGenerator _primeFactorGenerator = new PrimeFactorGenerator();
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void Generate_ShouldThrowExceptionForInvalidArguments(int number)
+        {
+            Action act = () => _primeFactorGenerator.Generate(number);
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void Generate_ShouldReturnNonEmptyList(int number)
+        {
+            _primeFactorGenerator.Generate(number).Should().NotBeEmpty();
+        }
+
+        [Theory]
+        [InlineData(2, new[] { 2 })]
+        [InlineData(3, new[] { 3 })]
+        [InlineData(4, new[] { 2, 2 })]
+        [InlineData(5, new[] { 5 })]
+        public void Generate_ShouldReturnCorrectList(int number, int[] expectedResult)
+        {
+            _primeFactorGenerator.Generate(number)
+                .ShouldAllBeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ PrimeFactors/PrimeFactors/PrimeFactorGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimeFactors
+{
+    public class PrimeFactorGenerator
+    {
+        public int[] Generate(int number)
+        {
+            if (number < 2)
+            {
+                throw new ArgumentException(nameof(number));
+            }
+            return InternalGenerate(number);
+        }
+
+        private int[] InternalGenerate(int number)
+        {
+            if (number < 2)
+            {
+                return new int[] {};
+            }
+            for (int i = 2; i <= number; i++)
+            {
+                if (number%i == 0)
+                {
+                    return new[] {i}
+                        .Concat(InternalGenerate(number/i))
+                        .ToArray();
+                }
+            }
+            throw new InvalidOperationException("Code should never be reached");
+        }
+    }
+}
