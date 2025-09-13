e8ce96499555d2ac00b8fd3a2b3d43cafb37b1cb
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-09-13 21:30:29 +0300

    Add SkyLine

---
+++ SkyLine/SkyLine.Tests/SkyLineCalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿namespace SkyLine.Tests;
+
+public sealed class SkyLineCalculatorTests
+{
+    public static object[][] GetTestCases() =>
+        new object[][]
+        {
+            [
+                new HouseInfo[]
+                {
+                    new(2, 9, 10),
+                    new(3, 7, 15),
+                    new(5, 12, 12),
+                    new(15, 20, 10),
+                    new(19, 24, 8),
+                },
+                new SkyLinePoint[]
+                {
+                    new(2, 10),
+                    new(3, 15),
+                    new(7, 12),
+                    new(12, 0),
+                    new(15, 10),
+                    new(20, 8),
+                    new(24, 0),
+                },
+            ],
+        };
+
+    [Theory]
+    [MemberData(nameof(GetTestCases))]
+    public void Calculate_ShouldCalculateCorrectSkyLine(
+        IReadOnlyList<HouseInfo> houses, IReadOnlyCollection<SkyLinePoint> expectedResult)
+    {
+        var result = SkyLineCalculator.CalculateSkyLine(houses);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ SkyLine/SkyLine/SkyLineCalculator.cs
@@ -0,0 +1,72 @@
+﻿namespace SkyLine;
+
+public sealed record HouseInfo(int Left, int Right, int Height);
+
+public sealed record SkyLinePoint(int X, int Height);
+
+public static class SkyLineCalculator
+{
+    public static IReadOnlyCollection<SkyLinePoint> CalculateSkyLine(
+        IReadOnlyList<HouseInfo> houses)
+    {
+        var borderGroups = houses
+            .SelectMany(GetHouseBorders)
+            .OrderBy(border => border.X)
+            .GroupBy(border => border.X)
+            .ToList();
+        var houseProcessed = new bool[houses.Count];
+        var activeBorders = new PriorityQueue<HouseBorder, int>(new ReverseComparer<int>());
+        var result = new List<SkyLinePoint>();
+        int? lastHeight = null;
+        foreach (var borderGroup in borderGroups)
+        {
+            foreach (var border in borderGroup)
+            {
+                if (border.BorderType == BorderType.Left)
+                {
+                    activeBorders.Enqueue(border, border.Height);
+                }
+                else
+                {
+                    houseProcessed[border.HouseIndex] = true;
+                }
+            }
+
+            while (activeBorders.TryPeek(out var border, out _) && houseProcessed[border.HouseIndex])
+            {
+                activeBorders.Dequeue();
+            };
+
+            var currentHeight = activeBorders.TryPeek(out _, out var height) ? height : 0;
+            if (lastHeight != currentHeight)
+            {
+                result.Add(new SkyLinePoint(borderGroup.Key, currentHeight));
+                lastHeight = currentHeight;
+            }
+        }
+        return result;
+
+    }
+
+    private static IReadOnlyCollection<HouseBorder> GetHouseBorders(
+        HouseInfo house, int houseIndex) =>
+    [
+        new(BorderType.Left, houseIndex, house.Left, house.Height),
+        new(BorderType.Right, houseIndex, house.Right, house.Height),
+    ];
+
+    private enum BorderType
+    {
+        Left,
+        Right,
+    }
+
+    private sealed record HouseBorder(
+        BorderType BorderType, int HouseIndex, int X, int Height);
+
+    private sealed class ReverseComparer<T> : IComparer<T> where T : IComparable<T>
+    {
+        public int Compare(T? x, T? y) => y?.CompareTo(x) ?? 0;
+    }
+}
+
