145167bca21aff0380f383c8776f0054705719b5
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-09-14 15:53:09 +0300

    Add tests

--- SkyLine/SkyLine.Tests/SkyLineCalculatorTests.cs
+++ SkyLine/SkyLine.Tests/SkyLineCalculatorTests.cs
@@ -6,6 +6,10 @@ public sealed class SkyLineCalculatorTests
         new object[][]
         {
             [
+                Array.Empty<HouseInfo>(),
+                Array.Empty<SkyLinePoint>(),
+            ],
+            [
                 new HouseInfo[]
                 {
                     new(2, 9, 10),
@@ -23,6 +27,39 @@ public sealed class SkyLineCalculatorTests
                     new(15, 10),
                     new(20, 8),
                     new(24, 0),
+                },
+            ],
+            [
+                new HouseInfo[]
+                {
+                    new(2, 9, 10),
+                    new(3, 7, 15),
+                    new(5, 12, 12),
+                    new(15, 20, 10),
+                    new(19, 24, 8),
+                },
+                new SkyLinePoint[]
+                {
+                    new(2, 10),
+                    new(3, 15),
+                    new(7, 12),
+                    new(12, 0),
+                    new(15, 10),
+                    new(20, 8),
+                    new(24, 0),
+                },
+            ],
+            [
+                new HouseInfo[]
+                {
+                    new(3, 6, 15),
+                    new(2, 3, 10),
+                    new(1, 3, 15),
+                },
+                new SkyLinePoint[]
+                {
+                    new(1, 15),
+                    new(6, 0),
                 },
             ],
         };
